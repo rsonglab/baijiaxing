5324d6a004c650c1d6d841c99546d6140a1bf1d7
Ronnie Song <ronniesong0809@gmail.com>
2021-05-12 01:10:22 -0700

    add a bjx() api, and a demo

---
+++ demo.js
@@ -0,0 +1,4 @@
+const bjx = require("./index")
+
+console.log(bjx())
+console.log(bjx.list)
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const list = require("./bjx.json");
+const { RandomInteger } = require("@rsonglab/random-generator");
+
+/**
+ * Returns a random Surnames
+ * @returns {String} Surnames
+ */
+module.exports = function () {
+  return list[RandomInteger(0, list.length - 1)];
+};
+
+/**
+ * A JSON array with all the Surnames
+ */
 
 module.exports.list = list;
