3a3334ff49fac36ff4d43f867af5c346149ab6a5
Ronnie Song <ronniesong0809@gmail.com>
2021-05-12 02:52:40 -0700

    use local random function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const list = require("./bjx.json");
-const { RandomInteger } = require("@rsonglab/random-generator");
+
+/**
+ * Returns a random number
+ * @returns {Number} random number
+ */
+function random(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
 
 /**
  * Returns a random Surnames
  * @returns {String} Surnames
  */
 module.exports = function () {
-  return list[RandomInteger(0, list.length - 1)];
+  return list[random(0, list.length - 1)];
 };
 
 /**
